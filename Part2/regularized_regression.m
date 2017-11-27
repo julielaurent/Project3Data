d1d956092db568561be4c3c9a5b8fb0912eac0b6
Alex Sebastiani <alexsebastiani@tsf-436-wpa-2-033.epfl.ch>
2017-11-27 16:43:17 +0100

    Alice

--- Part2/regularized_regression.m
+++ Part2/regularized_regression.m
@@ -15,7 +15,62 @@ testPosX = PosX(round(k*12862)+1:end,:);
 trainPosY = PosY(1:round(k*12862),:);
 testPosY = PosY(round(k*12862)+1:end,:);
 
-[trainSet_norm, mu, sigma] = zscore(trainSet);
-[coeff_pca, trainSet_pca, variance_pca] = pca(trainSet_norm);
+%[trainSet_norm, mu, sigma] = zscore(trainSet);
+%testSet_norm = ((testSet' - mu') ./ sigma')';
 
-testSet_pca = ((testSet' - mu') ./ sigma')' * coeff_pca;
+%% Linear Regression
+
+trainI_X = ones(size(trainPosX,1),1);
+trainI_Y = ones(size(trainPosY,1),1);
+
+testI_X = ones(size(testPosX,1),1);
+testI_Y = ones(size(testPosY,1),1);
+ 
+trainFM = trainSet;
+trainX_X_1 = [ trainI_X trainFM ];
+trainX_Y_1 = [ trainI_Y trainFM ];
+   
+testFM = testSet;
+testX_X_1 = [ testI_X testFM ];
+testX_Y_1 = [ testI_Y testFM ];
+ 
+% Regressor calculation
+b_X_1 = regress(trainPosX,trainX_X_1);
+b_Y_1 = regress(trainPosY,trainX_Y_1);
+
+% Train and test errors        
+trainErrX_1 = immse(trainPosX,trainX_X_1*b_X_1);
+trainErrY_1 = immse(trainPosY,trainX_Y_1*b_Y_1);   
+testErrX_1 = immse(testPosX,testX_X_1*b_X_1);
+testErrY_1 = immse(testPosY,testX_Y_1*b_Y_1);
+
+%% Plot
+
+% X
+figure('Color','w');
+subplot(2,1,1);
+title('Position Vector X');
+hold on;
+xlabel('Time');
+ylabel('PosX');
+plot(PosX,'-k','LineWidth',2);
+plot(trainX_X_1*b_X_1,'--b');
+plot(round(k*12862)+1:12862,testX_X_1*b_X_1,'-b');
+legend('Real position vector','Regressed position vector (train set)','Regressed position vector (test set)');
+box off;
+%axis([8800 9200 -0.05 0.2]);
+hold off;
+
+% Y
+subplot(2,1,2);
+title('Position Vector Y');
+hold on;
+xlabel('Time');
+ylabel('PosY');
+plot(PosY,'-k','LineWidth',2);
+plot(trainX_Y_1*b_Y_1,'--b');
+plot(round(k*12862)+1:12862,testX_Y_1*b_Y_1,'-b');
+box off;
+%axis([8800 9200 0.15 0.3]);
+hold off;
+
